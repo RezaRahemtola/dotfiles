a237f1473e848790f71d9c83fa20c4cedd797752
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-10-08 21:48:32 +0200

    feat: Global gitignore

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 rm -f ~/.gitconfig
 ln -s $(pwd)/git/.gitconfig ~/.gitconfig
 
+rm -f ~/.gitignore
+ln -s $(pwd)/git/.gitignore ~/.gitignore
+
 rm -f ~/.config/Code/User/settings.json
 ln -s $(pwd)/vscode/settings.json ~/.config/Code/User/settings.json
 
