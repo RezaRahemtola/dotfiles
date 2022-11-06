726f6809dfd115e79d7d96337756bbbbb0e3cd94
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-11-06 02:11:00 +0100

    feat: VSCode keybinding for filename search

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ ln -s $(pwd)/git/.gitignore ~/.gitignore
 
 rm -f ~/.config/Code/User/settings.json
 ln -s $(pwd)/vscode/settings.json ~/.config/Code/User/settings.json
+rm -f ~/.config/Code/User/keybindings.json
+ln -s $(pwd)/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 
 rm -rf ~/.config/fish
 ln -s $(pwd)/fish ~/.config/fish
