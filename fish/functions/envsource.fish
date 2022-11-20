c29f46743320c7b6454fc5ab7fca9f7ab7319d98
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-11-20 22:39:43 +0100

    fix: Better envsource function for Fish

--- fish/functions/envsource.fish
+++ fish/functions/envsource.fish
@@ -3,9 +3,5 @@
 # Usage: envsource <path/to/env>
 
 function envsource
-  for line in (cat $argv | grep -v '^#' | grep -v '^\s*$')
-    set item (string split -m 1 '=' $line)
-    set -gx $item[1] $item[2]
-    echo "Exported key $item[1]"
-  end
+  exec bash -c "source $argv; exec fish"
 end
