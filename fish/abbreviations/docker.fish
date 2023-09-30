d1f5c0e6a530d057f3012e3e906c7e5ffe08f931
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-09-30 21:53:05 +0200

    feat(fish): docker rmv abbreviation

--- fish/abbreviations/docker.fish
+++ fish/abbreviations/docker.fish
@@ -2,3 +2,9 @@
 abbr -a -- dcd 'docker compose down'
 abbr -a -- dcda 'docker compose down --remove-orphans --rmi all -v'
 abbr -a -- dcu 'docker compose up'
+
+function docker_rmv
+    string match --quiet 'docker rmv ' -- (commandline -j); or return 1
+    echo "volume rm "
+end
+abbr -a docker_rmv --position anywhere --regex rmv --function docker_rmv
