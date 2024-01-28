a42ad42dd04fcc1b8fb827079555bdf8a724b726
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-01-28 22:39:10 +0100

    fix(fish): docker compose up alias with detached by default

--- fish/abbreviations/docker.fish
+++ fish/abbreviations/docker.fish
@@ -2,7 +2,7 @@
 abbr -a -- dcb 'docker compose build'
 abbr -a -- dcd 'docker compose down'
 abbr -a -- dcda 'docker compose down --remove-orphans --rmi all -v'
-abbr -a -- dcu 'docker compose up'
+abbr -a -- dcu 'docker compose up -d'
 abbr -a -- dps 'docker ps'
 abbr -a -- dpsa 'docker ps -a'
 
