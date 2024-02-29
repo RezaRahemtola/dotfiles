79dd728799e6b0a3ce42b23ca9ef842bde2c1388
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-02-29 23:36:22 +0100

    feat(fish): Remove unused functions

--- fish/functions/gotorepo.fish
+++
@@ -1,3 +0,0 @@
-function gotorepo --description 'cd into a given folder name'
-    cd (find ~/poc ~/delivery ~/Documents -iname "$argv" -type "d" | head -n 1)
-end
--- fish/functions/openrepo.fish
+++
@@ -1,3 +0,0 @@
-function openrepo --description 'open a given folder in the default IDE'
-	$VISUAL (find ~/poc ~/delivery ~/Documents -iname "$argv" -type "d" | head -n 1)
-end
